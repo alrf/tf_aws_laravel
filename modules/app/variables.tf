83060e71d8a1f9dd4a3600dea1b7bf2bf012e6b6
AlexF <alexander.fokin@genesys.com>
2019-08-21 18:35:54 +0400

    deleted unused module

--- modules/app/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-variable "cloudfront_domain_name" {}
-variable "db_host" {}
-variable "db_name" {}
-variable "db_username" {}
-variable "db_password" {}
